39fb571e53059a12bb6951f17e79a2d2602c5602
Eric Leitich <75421829+Leitich@users.noreply.github.com>
2022-09-21 05:29:55 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php 
+
+session_start();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+</head>
+<body style="background-image:background.jpg;" >
+    <h1 style="text-align:center ;" >This is the Landing Page</h1>
+    <br><a href="signup.php" style="font-size: 2rem ;">Sign Up</a>
+    <br><a href="login.php" style="font-size: 2rem ;">Login</a>
+    
+</body>
+</html>
